c9447fae0369a442daeb4a0c0634f239beac4f08
isak <isakedo@gmail.com>
2018-02-23 13:46:12 +0100

    initial commit

---
+++ j--/tests/pass/Division.java
@@ -0,0 +1,18 @@
+package pass;
+
+import java.lang.System;
+
+public class Division {
+	public int divide(int x, int y) {
+		return x / y;
+	}
+	
+    public static void main(String[] args) {
+    	int x = n1;
+    	int y = n2;
+        System.out.println(x + " / " + y + " = " + x / y);
+    }
+    
+    static int n1 = 120;
+    static int n2 = 4;
+}
---
+++ j--/tests/pass/Remainder.java
@@ -0,0 +1,18 @@
+package pass;
+
+import java.lang.System;
+
+public class Remainder {
+	public int remainder(int x, int y) {
+		return x / y;
+	}
+	
+    public static void main(String[] args) {
+    	int x = n1;
+    	int y = n2;
+        System.out.println(x + " % " + y + " = " + x % y);
+    }
+    
+    static int n1 = 127;
+    static int n2 = 4;
+}
